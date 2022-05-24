778c0a3215fb8d09185998d927f745267a323100
mici <60635066+midorimici@users.noreply.github.com>
2022-05-25 00:40:00 +0900

    Server: Add echo

--- server/main.go
+++ server/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+)
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Logger.SetLevel(log.INFO)
+
+	front_url := os.Getenv("FRONT_URL")
+	if front_url == "" {
+		front_url = "http://localhost:3000"
+	}
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{front_url},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
+
+	// Routes
+
+	// Start server
+	e.Logger.Fatal(e.Start(":9000"))
+}
