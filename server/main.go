488ec8f61d51f7ea9892e5720b6ef62992e4ff4b
nanako-kimura <kimura-nanako71@g.ecc.u-tokyo.ac.jp>
2022-05-25 23:01:25 +0900

    create database and return posts

--- server/main.go
+++ server/main.go
@@ -1,13 +1,56 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+var DbConnection *sql.DB
+
+type Post struct {
+	Name string `json:"name"`
+	Content string `json:"category"`
+	Tag string `json:"image"`
+}
+
+func addPost(c echo.Context) error {
+	return c.String(http.StatusOK, "OK")
+}
+
+func showPost(c echo.Context) error {
+	DbConnection,_:=sql.Open("sqlite3","mercari.sqlite3")
+	defer DbConnection.Close()
+
+	cmd := "SELECT name,content,tag FROM post"
+	rows, err := DbConnection.Query(cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	
+	var posts []Post
+	for rows.Next(){
+		var post Post
+		err := rows.Scan(&post.Name,&post.Content,&post.Tag)
+		if err != nil{
+			log.Fatal(err)
+		}
+		posts = append(posts,post)
+	}
+	n_json, err := json.Marshal(posts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c.String(http.StatusOK, string(n_json))
+}
 
 func main() {
 	e := echo.New()
@@ -29,5 +72,7 @@ func main() {
 	// Routes
 
 	// Start server
+	e.POST("/post", addPost)
+	e.GET("/post", showPost)
 	e.Logger.Fatal(e.Start(":9000"))
 }
