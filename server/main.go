128dbf7e140e6bd1988d49ddce92fe54a924db43
Shiori Yoshida <g1920543@is.ocha.ac.jp>
2022-05-28 22:09:34 +0900

    category

--- server/main.go
+++ server/main.go
@@ -1,33 +1,67 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+var DbConnection *sql.DB
+
+type Post struct {
+	Name string `json:"name"`
+	Category string `json:"category"`
+	Content string `json:"content"`
+	Tag string `json:"tags"`
+}
+
+func addPost(c echo.Context) error {
+	return c.String(http.StatusOK, "OK")
+}
+
+func showPosts(c echo.Context) error {
+	DbConnection,_:=sql.Open("sqlite3","mercari.sqlite3")
+	defer DbConnection.Close()
+
+	cmd := "SELECT name,category,content,tag FROM post"
+	rows, err := DbConnection.Query(cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var posts []Post
+	for rows.Next(){
+		var post Post
+		err := rows.Scan(&post.Name,&post.Category,&post.Content,&post.Tag)
+		if err != nil{
+			log.Fatal(err)
+		}
+		posts = append(posts,post)
+	}
+	n_json, err := json.Marshal(posts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c.String(http.StatusOK, string(n_json))
+}
 
 func main() {
 	e := echo.New()
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Logger.SetLevel(log.INFO)
-
-	front_url := os.Getenv("FRONT_URL")
-	if front_url == "" {
-		front_url = "http://localhost:3000"
-	}
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{front_url},
-		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+@@ -27,6 +70,8 @@ func main() {
 	}))
 
 	// Routes
+	e.POST("/posts", addPost)
+	e.GET("/posts", showPosts)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
-}
