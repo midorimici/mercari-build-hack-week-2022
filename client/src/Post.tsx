2337d5fea88a38a4a7105166cb49e08206f7b652
mici <60635066+midorimici@users.noreply.github.com>
2022-05-29 20:39:46 +0900

    Refactor

--- client/src/Post.tsx
+++ client/src/Post.tsx
@@ -1,82 +1,75 @@
-import { useState } from "react";
-import { isConditionalExpression } from "typescript";
+import { useState } from 'react';
 
 const initialState = {
-	name: "",
-	category: "",
-	content: "",
-	tags: "",
+  name: '',
+  category: '',
+  content: '',
+  tags: '',
 };
 
-const server = process.env.API_URL || "http://127.0.0.1:9000";
+const server = process.env.API_URL || 'http://localhost:9000';
 
 export function Post() {
-	const [values, setValues] = useState(initialState);
+  const [values, setValues] = useState(initialState);
 
-	const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setValues({ ...values, [event.target.name]: event.target.value });
-	};
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
 
-	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		const data = new FormData();
-		data.append("name", values.name);
-		data.append("category", values.category);
-		data.append("content", values.content);
-		data.append("tags", values.tags);
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append('name', values.name);
+    data.append('category', values.category);
+    data.append('content', values.content);
+    data.append('tags', values.tags);
 
-		fetch(server.concat("/posts"), {
-			method: "POST",
-			mode: "cors",
-			body: data,
-		})
-			.then((response) => response.statusText)
-			.then((data) => {
-				console.log("POST success:", data);
-				setValues({ ...values, name: "" });
-				console.log(values);
-			})
-			.catch((error) => {
-				console.error("POST error:", error);
-			});
-	};
+    fetch(server.concat('/posts'), {
+      method: 'POST',
+      mode: 'cors',
+      body: data,
+    })
+      .then((response) => response.statusText)
+      .then((data) => {
+        console.log('POST success:', data);
+        setValues({ ...values, name: '' });
+        console.log(values);
+      })
+      .catch((error) => {
+        console.error('POST error:', error);
+      });
+  };
 
-	return (
-		<div className="Listing">
-			<form onSubmit={onSubmit}>
-				<div>
-					<input
-						type="text"
-						name="name"
-						id="name"
-						placeholder="name"
-						onChange={onChange}
-						required
-					/>
-					<input
-						type="text"
-						name="category"
-						id="category"
-						placeholder="category"
-						onChange={onChange}
-					/>
-					<input
-						type="text"
-						name="content"
-						id="content"
-						placeholder="content"
-						onChange={onChange}
-					/>
-					<input
-						type="text"
-						name="tags"
-						id="tags"
-						placeholder="tags"
-						onChange={onChange}
-					/>
-					<button type="submit">Post</button>
-				</div>
-			</form>
-		</div>
-	);
+  return (
+    <div className="Listing">
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="name"
+            onChange={onChange}
+            required
+          />
+          <input
+            type="text"
+            name="category"
+            id="category"
+            placeholder="category"
+            onChange={onChange}
+          />
+          <input
+            type="text"
+            name="content"
+            id="content"
+            placeholder="content"
+            onChange={onChange}
+          />
+          <input type="text" name="tags" id="tags" placeholder="tags" onChange={onChange} />
+          <button type="submit">Post</button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- client/src/PostList.tsx
+++ client/src/PostList.tsx
@@ -1,72 +1,67 @@
-import React, { useState, useEffect } from "react";
-import { ReplyDialog } from "./components/ReplayDialog";
-import "./PostList.css";
+import { useState, useEffect } from 'react';
+import { ReplyDialog } from './components/ReplayDialog';
+import './PostList.css';
 
 interface Item {
-	name: string;
-	category: string;
-	content: string;
-	tags: string;
+  name: string;
+  category: string;
+  content: string;
+  tags: string;
 }
 
-type Props = {
-	isVisible: boolean;
-};
-
-const server = process.env.API_URL || "http://127.0.0.1:9000";
+const server = process.env.API_URL || 'http://localhost:9000';
 
 export function PostList() {
-	const [items, setItems] = useState<Item[]>([]);
-	const [visible, setVisible] = useState<boolean>(false);
+  const [items, setItems] = useState<Item[]>([]);
+  const [visible, setVisible] = useState<boolean>(false);
 
-	const fetchItems = () => {
-		fetch(server.concat("/posts"), {
-			method: "GET",
-			mode: "cors",
-			headers: {
-				"Content-Type": "application/json",
-				Accept: "application/json",
-			},
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log("GET success:", data);
-				setItems(data);
-			})
-			.catch((error) => {
-				console.error("GET error:", error);
-			});
-	};
+  const fetchItems = () => {
+    fetch(server.concat('/posts'), {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('GET success:', data);
+        setItems(data);
+      })
+      .catch((error) => {
+        console.error('GET error:', error);
+      });
+  };
 
-	useEffect(() => {
-		fetchItems();
-	}, []);
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-	const postlist = (
-		<div>
-			{items.map((item) => {
-				return (
-					<div key={item.name} className="ItemList">
-						{/* TODO: Task 1: Replace the placeholder image with the item image */}
-						<div className="Info">
-							<div className="Box">Name: {item.name}</div>
-							<div className="Box">Category: {item.category}</div>
-							<div className="Box">tag: {item.tags}</div>
-						</div>
-						<div className="Container">
-							<div className="Content">content: {item.content}</div>
-							<button onClick={() => setVisible(true)}>紹介</button>
-						</div>
-					</div>
-				);
-			})}
-		</div>
-	);
+  const postlist = (
+    <div>
+      {items.map((item, index) => {
+        return (
+          <div key={index} className="ItemList">
+            <div className="Info">
+              <div className="Box">Name: {item.name}</div>
+              <div className="Box">Category: {item.category}</div>
+              <div className="Box">tag: {item.tags}</div>
+            </div>
+            <div className="Container">
+              <div className="Content">content: {item.content}</div>
+              <button onClick={() => setVisible(true)}>紹介</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 
-	return (
-		<div>
-			{postlist}
-			<ReplyDialog isVisible={visible} />
-		</div>
-	);
+  return (
+    <div>
+      {postlist}
+      <ReplyDialog isVisible={visible} />
+    </div>
+  );
 }
