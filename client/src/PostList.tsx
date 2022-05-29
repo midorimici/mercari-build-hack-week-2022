e81c07eb11209c6aea1832a0d401c1f248fd01b9
nanako-kimura <kimura-nanako71@g.ecc.u-tokyo.ac.jp>
2022-05-29 15:35:45 +0900

    change

--- client/src/PostList.tsx
+++ client/src/PostList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface Item {
     name: string;
@@ -7,30 +7,56 @@ interface Item {
     tag: string
   };
 
+const server = process.env.API_URL || 'http://127.0.0.1:9000';
 
 export function PostList(){
     
     const [items, setItems] = useState<Item[]>([])
+    const fetchItems = () => {
+      fetch(server.concat('/posts'),
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers : {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('GET success:',data);
+          setItems(data.items);
+        })
+        .catch(error => {
+          console.error('GET error:',error)
+        })
+    }
+
+    useEffect(() => {
+      fetchItems();
+    }, []);
 
 
-        return (
-            <div>
-              { items.map((item) => {
-                return (
-                  <div key={item.name} className='ItemList'>
-                    {/* TODO: Task 1: Replace the placeholder image with the item image */}
-                    <p>
-                    <span>Name: {item.name}</span>
-                    <br/>
-                    <span>Category: {item.category}</span>
-                    <br/>
-                    <span>content: {item.content}</span>
-                    </p>
-                  </div>
-                )
-              })}
-            </div>
-          )
+    return (
+        <div>
+          { items.map((item) => {
+            return (
+              <div key={item.name} className='ItemList'>
+                {/* TODO: Task 1: Replace the placeholder image with the item image */}
+                <p>
+                <span>Name: {item.name}</span>
+                <br/>
+                <span>Category: {item.category}</span>
+                <br/>
+                <span>content: {item.content}</span>
+                <br/>
+                <span>tag: {item.tag}</span>
+                </p>
+              </div>
+            )
+          })}
+        </div>
+      )
 
       
     }
